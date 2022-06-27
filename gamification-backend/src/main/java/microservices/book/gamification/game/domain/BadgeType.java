3ee7458c8768780d4c18708feb0f62239e0a5b49
mthsps <dmatheusps@gmail.com>
2022-06-27 18:17:34 -0300

    Chapter 6, Section: Domain

---
+++ gamification-backend/src/main/java/microservices/book/gamification/game/domain/BadgeType.java
@@ -0,0 +1,21 @@
+package microservices.book.gamification.game.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Enumeration with the different types of Badges that a user can win.
+ */
+@RequiredArgsConstructor
+@Getter
+public enum BadgeType {
+
+    BRONZE("Bronze"),
+    SILVER("Silver"),
+    GOLD("Gold"),
+    FIRST_WON("First time"),
+    LUCKY_NUMBER("Lucky number");
+
+    private final String description;
+
+}
