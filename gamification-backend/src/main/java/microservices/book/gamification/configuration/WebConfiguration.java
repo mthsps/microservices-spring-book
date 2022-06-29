c5fd3e0acbf3df661618b197a45d1bba8f73a006
mthsps <dmatheusps@gmail.com>
2022-06-28 21:19:03 -0300

    Chapter 6, Section: Configuration

---
+++ gamification-backend/src/main/java/microservices/book/gamification/configuration/WebConfiguration.java
@@ -0,0 +1,12 @@
+package microservices.book.gamification.configuration;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
+}
--- gamification-backend/src/main/java/microservices/book/gamification/game/BadgeRepository.java
+++ gamification-backend/src/main/java/microservices/book/gamification/game/BadgeRepository.java
@@ -8,5 +8,12 @@ import java.util.List;
 import java.util.Set;
 
 public interface BadgeRepository extends JpaRepository<BadgeCard, Long> {
+
+    /**
+     * Retrieves all BadgeCards for a given user.
+     *
+     * @param userId the id of the user to look for BadgeCards
+     * @return the list of BadgeCards, sorted by most recent.
+     */
     List<BadgeCard> findByUserIdOrderByBadgeTimestampDesc(final Long userId);
 }
