b0b439ba6d49e0796fbc01cd30b100801a5cec6c
mthsps <dmatheusps@gmail.com>
2022-07-10 10:12:30 -0300

    Chapter 8, Sections: Centralized Logs and Distributed Tracing

---
+++ logs/src/test/java/book/microservices/logs/LogsApplicationTests.java
@@ -0,0 +1,13 @@
+package book.microservices.logs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- multiplication-backend/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptController.java
+++ multiplication-backend/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptController.java
@@ -18,6 +18,7 @@ public class ChallengeAttemptController {
 
     @PostMapping
     ResponseEntity<ChallengeAttempt> postResult(@RequestBody @Valid ChallengeAttemptDTO challengeAttemptDTO) {
+        log.info("Received new attempt from {}", challengeAttemptDTO.getUserAlias());
         return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDTO));
     }
 
